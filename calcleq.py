f1389ab6b80d512751be0f642b7a3ac10f971097
ana <anuskycuevas@gmail.com>
2017-10-21 20:30:04 +0200

    funciona, pero faltan arreglos

---
+++ calcleq.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoo
+import csv
+from decimal import *
+import math
+
+
+class Calcdb:
+    
+    def __init__(self, valor):
+        self.valor = valor
+
+    def a_lineal(self):
+        
+        getcontext().prec = 10
+        self.valor = float(Decimal(10)**Decimal(self.valor/10))
+        return(self.valor)
+        
+    def a_db(self):
+        self.valor = math.log10(self.valor)
+        return 10*(self.valor)
+        
+if __name__ == "__main__":
+    getcontext().prec = 10
+    if len(sys.argv) != 2:
+        sys.exit("Input: python3 calcplus.py fichero")
+
+    with open(sys.argv[1], newline='') as f:
+        Fich = csv.reader(f)
+        for Linea in Fich:
+            Muestras = Linea[0]
+            Total = 0
+            Operandos = Linea[1:]
+            for Operando in Operandos:
+                Operar = Calcdb(float(Operando))
+                Valor = Operar.a_lineal()
+
+                Total= Total + Valor
+            Operar = calcoo.Calculadora(float(Total),float(Muestras))
+
+            Total = Operar.divide()
+            Operar = Calcdb(float(Total))
+            print(Operar.a_db())
